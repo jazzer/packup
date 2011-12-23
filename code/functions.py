9dbfb3affac2f15f64d54d0778bdc467eae40386
Johannes Mitlmeier <github@spam.slzm.de>
2011-12-24 00:58:38 +0100

    Implement first few preconditions )wireless, mounted directory, time)

--- code/functions.py
+++ code/functions.py
@@ -1,17 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import subprocess
+import os.path
+import datetime
+
 
 # define preconditions
 def isDirectoryMounted(path):
-    return True
+    print('Checking if path "%s" is mounted' % path)
+    if os.path.exists(path):
+        print('It is mounted.')
+        return True
+    else:
+        print('It is not mounted.')
+        return False
+
 
 def isWirelessAvailible(SSID):
-    return True
+    print('Checking if wireless network named "%s" is availible' % SSID)
+    output = getCommandOutput('sudo iwlist scan')
+    #print(output)
+    if output.find('ESSID:"%s"' % SSID) > -1:
+        print('It is availible.')
+        return True
+    else:
+        print('It is not availible.')
+        return False
 
 def isWirelessConnected(SSID):
-    return True
+    print('Checking if we are connected to a wireless network named "%s"' % SSID)
+    output = getCommandOutput('iwconfig')
+    #print(output)
+    if output.find('ESSID:"%s"' % SSID) > -1:
+        print('We are connected.')
+        return True
+    else:
+        print('We are not connected.')
+        return False
 
+# time of day related
+def afterHourOfDay(hour):
+    print('Checking if it is later than %s o\'clock' % hour)
+    if datetime.datetime.now().hour >= hour:
+        print('Time is okay.')
+        return True
+    else:
+        print('Too early.')
+        return False
+
+# backup frequency related
 def notBackuppedToday():
     return False
 
@@ -29,5 +67,10 @@ def notifyByEMail():
 
 
 # backup functions
-def callRsnapshot(timeframe):
+def callRsnapshot(timecode):
     return True
+
+
+# python/linux binding
+def getCommandOutput(command):
+    return subprocess.getoutput(command)
--- packup.py
+++ packup.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import code.functions
+import code.functions 
 import argparse
+import sys
 
 # parse parameters
-parser = argparse.ArgumentParser(description='Backup script based on Unix magic and python.')
+parser = argparse.ArgumentParser(description='Backup script based on rsnapshot, Unix magic, and python.')
 # TODO implement (verbose, dry-run, ...)
 
+debug = True
 
 # check if preconditions are met
+if not code.functions.isDirectoryMounted('/home'):
+    sys.exit(1)
+if not code.functions.isWirelessConnected('MiNetz'):
+    sys.exit(1)
+if not code.functions.isWirelessAvailible('MiNetz'):
+    sys.exit(1)
+if not code.functions.afterHourOfDay(10):
+    sys.exit(1)
+
+# Popen(args, bufsize=0, executable=None, stdin=None, stdout=None, stderr=None, preexec_fn=None, close_fds=False, shell=False, cwd=None, env=None, universal_newlines=False, startupinfo=None, creationflags=0)
